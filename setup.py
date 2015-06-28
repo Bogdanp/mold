da5bfb12674f88befeb8b2fe6fc8bc9a7827da99
Bogdan Popa <popa.bogdanp@gmail.com>
2015-06-28 13:22:40 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,16 +2,11 @@ from setuptools import setup
 
 setup(
     name="pymold",
-    version="0.1.0",
+    version="0.1.1",
     description="A simple, fast template engine for Python.",
     long_description="https://github.com/Bogdanp/mold",
-    packages=[
-        "mold",
-    ],
-
-    install_requires=[
-    ],
-
+    packages=["mold"],
+    install_requires=[],
     author="Bogdan Popa",
     author_email="popa.bogdanp@gmail.com",
     url="https://github.com/Bogdanp/mold",
